d2fed6ae2e9d5f65c5c411c7a0656dd6b8ae072e
Jeciel Barraza <bjecielz@gmail.com>
2019-05-31 15:53:31 -0500

    Random
    Agragado random para inicializar de manera automatica el array

--- INSERTION-SORT.cpp
+++ INSERTION-SORT.cpp
@@ -3,12 +3,25 @@ INSERTION-SORT
 */
 
 #include <iostream>
+#include <stdlib.h>
 
 using namespace std;
 
 int main()
 {
-    int A[] = {5, 2, 4, 6, 1, 3, 7}, n=7, key=0;
+    
+    /* iniciar semilla del random */   
+    srand(time(NULL));
+    
+    int n;
+    cin>>n;
+    
+    
+    int A[n], key=0;
+    
+    for(int i = 0; i<n; i++){
+        A[i] = rand() % 50 +1;
+    }
     
     for(int j = 1, i = 0; j<n; j++){
         key = A[j];
@@ -21,7 +34,7 @@ int main()
     }
     
     for(int i = 0; i<n; i++){
-        cout<<A[i];
+        cout<<A[i]<<((i!=n-1)?", ":" ");
     }
     cout<<""<<endl;
     return 0;
